7a10d7f79eda93d4b2abbac3e58d1474a64549a0
Jean-Sébastien Leroy <jean.sebastien.leroy@gmail.com>
2024-11-11 07:57:46 +0100

    update nvim configuration

--- init.lua
+++ init.lua
@@ -7,6 +7,15 @@ vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 vim.opt.wrap = false
+vim.opt.textwidth = 80
+vim.opt.numberwidth = 4   -- (up to 9999)
+vim.opt.scrolloff = 4     -- Minimum number of lines to keep above and below the cursor
+vim.opt.tabstop = 2       -- Number of columns occupied by a tab
+vim.opt.softtabstop = 2   -- See multiple spaces as tabstops so <BS> does the right thing
+vim.opt.expandtab = true  -- Converts tabs to white space
+vim.opt.shiftwidth = 2    -- Width for autoindents
+vim.opt.backup = false
+vim.opt.swapfile = false
 
 -- Make line numbers default
 vim.opt.number = true
@@ -37,7 +46,7 @@ vim.opt.smartcase = true
 vim.opt.signcolumn = 'yes'
 
 -- Decrease update time
-vim.opt.updatetime = 250
+vim.opt.updatetime = 100
 
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
@@ -84,20 +93,6 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 -- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -112,16 +107,6 @@ vim.opt.rtp:prepend(lazypath)
 
 
 -- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
@@ -130,6 +115,13 @@ require('lazy').setup({
   'dcharbon/vim-flatbuffers',
   'vhda/verilog_systemverilog.vim',
   'lewis6991/gitsigns.nvim',
+
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
 
   {
     "echasnovski/mini.align",
@@ -142,7 +134,6 @@ require('lazy').setup({
     },
   },
 
-
   { 'akinsho/toggleterm.nvim',
     opts = {
       open_mapping = [[<leader>t]],
@@ -150,99 +141,7 @@ require('lazy').setup({
   },
 
   {
-    "vague2k/huez.nvim",
-    import = "huez-manager.import",
-    branch = "stable",
-    event = "UIEnter",
-    config = function()
-      require("huez").setup({})
-    end,
-  },
-
-  -- {
-  --   "echasnovski/mini.icons",
-  --   opts = {},
-  --   lazy = true,
-  --   specs = {
-  --     { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
-  --   },
-  --   init = function()
-  --     package.preload["nvim-web-devicons"] = function()
-  --       require("mini.icons").mock_nvim_web_devicons()
-  --       return package.loaded["nvim-web-devicons"]
-  --     end
-  --   end,
-  -- },
-
-  -- {
-  --   'nvim-lualine/lualine.nvim',
-  --   config = function()
-  --     local lualine = require 'lualine'
-  --     local icon = require 'nvim-web-devicons'
-  --     local icon = require 'hieulw.icons'
-  --     local mode = "mode"
-  --     local filetype = { "filetype", icon_only = true }
-
-  --     local diagnostics = {
-  --       "diagnostics",
-  --       sources = { "nvim_diagnostic" },
-  --       sections = { "error", "warn", "info", "hint" },
-  --       symbols = {
-  --         error = icon.diagnostics.Error,
-  --         hint = icon.diagnostics.Hint,
-  --         info = icon.diagnostics.Info,
-  --         warn = icon.diagnostics.Warning,
-  --       },
-  --       colored = true,
-  --       update_in_insert = false,
-  --       always_visible = false,
-  --     }
-
-  --     local diff = {
-  --       "diff",
-  --       source = function()
-  --         local gitsigns = vim.b.gitsigns_status_dict
-  --         if gitsigns then
-  --           return {
-  --             added = gitsigns.added,
-  --             modified = gitsigns.changed,
-  --             removed = gitsigns.removed,
-  --           }
-  --         end
-  --       end,
-  --       symbols = {
-  --         added = icon.git.LineAdded .. " ",
-  --         modified = icon.git.LineModified .. " ",
-  --         removed = icon.git.LineRemoved .. " ",
-  --       },
-  --       colored = true,
-  --       always_visible = false,
-  --     }
-
-  --     lualine.setup {
-  --       options = {
-  --         theme = "auto",
-  --         globalstatus = true,
-  --         section_separators = "",
-  --         component_separators = "",
-  --         disabled_filetypes = { statusline = { "dashboard", "lazy", "alpha" } },
-  --       },
-  --       sections = {
-  --         lualine_a = { mode },
-  --         lualine_b = {},
-  --         lualine_c = { "filename" },
-  --         lualine_x = { diff, diagnostics, filetype },
-  --         lualine_y = {},
-  --         lualine_z = {},
-  --       },
-  --     }
-  --   end,
-  -- },
-
-  {
     'MeanderingProgrammer/render-markdown.nvim',
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     opts = {},
   },
@@ -423,7 +322,9 @@ require('lazy').setup({
       },
     },
   },
+
   { 'Bilal2453/luvit-meta', lazy = true },
+
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
@@ -549,7 +450,6 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        -- gopls = {},
         pyright = {},
         bashls = {},
         cmake = {},
@@ -558,38 +458,9 @@ require('lazy').setup({
         jsonls = {},
         markdown_oxide = {},
         yamlls = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
-        lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
-        },
+        lua_ls = {},
       }
 
-      -- Ensure the servers and tools above are installed
-      --  To check the current status of installed tools and/or manually install
-      --  other tools, you can run
-      --    :Mason
-      --
-      --  You can press `g?` for help in this menu.
       require('mason').setup()
 
       -- You can add other tools here that you want Mason to install
@@ -619,37 +490,9 @@ require('lazy').setup({
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
-      },
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds other completion capabilities.
-      --  nvim-cmp does not ship with all sources by default. They are split
-      --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
     },
     config = function()
       -- See `:help cmp`
@@ -658,11 +501,6 @@ require('lazy').setup({
       luasnip.config.setup {}
 
       cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
         completion = { completeopt = 'menu,menuone,noinsert' },
 
         -- For an understanding of why these mappings were
@@ -677,28 +515,6 @@ require('lazy').setup({
           ['<Tab>'] = cmp.mapping.select_next_item(),
           ['<S-Tab>'] = cmp.mapping.select_prev_item(),
           ['<C-Space>'] = cmp.mapping.complete {},
-
-          -- Think of <c-l> as moving to the right of your snippet expansion.
-          --  So if you have a snippet that's like:
-          --  function $name($args)
-          --    $body
-          --  end
-          --
-          -- <c-l> will move you to the right of each of the expansion locations.
-          -- <c-h> is similar, except moving you backwards.
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-
-          -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-          --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
         },
         sources = {
           {
@@ -772,19 +588,10 @@ require('lazy').setup({
       auto_install = true,
       highlight = {
         enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 }, {
   ui = {
@@ -808,6 +615,6 @@ require('lazy').setup({
   },
 })
 
--- vim.cmd.colorscheme "tokyonight"
+vim.cmd.colorscheme "tokyonight"
 
 -- vim: ts=2 sts=2 sw=2 et
