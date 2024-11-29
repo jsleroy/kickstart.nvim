99d53f0cd6e21ea56ea5e3f01abc549d43660f49
Jean-Sébastien Leroy <jean.sebastien.leroy@gmail.com>
2024-11-29 17:45:27 +0100

    update nvim config

--- init.lua
+++ init.lua
@@ -214,6 +214,18 @@ require('lazy').setup({
         bind_to_cwd = false,
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            -- '.git',
+            -- '.DS_Store',
+            -- 'thumbs.db',
+          },
+          never_show = {},
+        },
       },
       window = {
         mappings = {
